34af6daee73f8877e90f34a65c6b925c03b561a0
Justus Klausecker <justus@klausecker.de>
2024-10-21 00:57:28 +0200

    fix git ignoring file name casing

---
+++ src/game.zig
@@ -0,0 +1,6 @@
+//! Provides a framework for keeping track of gamestate and handling user inputs.
+
+pub const State = @import("game/State.zig");
+pub const Round = @import("game/Round.zig");
+pub const Map = @import("game/Map.zig");
+pub const ThreadPool = @import("game/ThreadPool.zig");
